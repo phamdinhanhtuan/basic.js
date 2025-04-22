8c1a8686cf5feda926d755a009924a56b6a20c8f
PHẠM ĐÌNH ANH TUẤN <143309309+phamdinhanhtuan@users.noreply.github.com>
2025-04-22 23:05:43 +0700

    bangCuuChuong.js

---
+++ bangCuuChuong.js
@@ -0,0 +1,7 @@
+for (let i = 2; i <= 9; i++) {
+  console.log(`📘 Bảng nhân ${i}:`);
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`);
+  }
+  console.log("----------");
+}
