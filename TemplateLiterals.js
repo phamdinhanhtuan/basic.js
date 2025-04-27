449215f46da1c3dbaf185283d75136becce2b21e
PHẠM ĐÌNH ANH TUẤN <143309309+phamdinhanhtuan@users.noreply.github.com>
2025-04-27 23:05:55 +0700

    TemplateLiterals.js

---
+++ TemplateLiterals.js
@@ -0,0 +1,7 @@
+const courseName = "JavaScript";
+const courseName2 = "Java";
+const description = `courseName:${courseName}`;
+
+const Name = "tuan";
+const fullName = `Name:${Name}`;
+console.log(fullName);
